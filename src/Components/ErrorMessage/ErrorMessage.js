2db45679acff3d77cb9209c8df42679a45b62d2e
TIit0 <viverlin7@gmail.com>
2023-01-25 13:24:24 -0600

    Updates

--- src/Components/ErrorMessage/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import "./ErrorMessage.css"
-
-export default function ErrorMessage({children}) {
-    return (
-        <div className="error-container">
-            <h2>{children}</h2>
-        </div>
-    );
-}
